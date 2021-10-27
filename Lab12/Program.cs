b43c4ee9dcaf7447924a3d7bd7b57d14137e0b85
Алексей <@DESKTOP-V0RD6HJ>
2021-10-27 12:19:07 +0700

    Добавьте файлы проекта.

---
+++ Lab12/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Program.Сir.Per(10));
+            Console.WriteLine(Program.Сir.Sq(10));
+            Console.WriteLine(Program.Сir.Aff(10, 15, 18));
+            Console.ReadKey();
+        }
+        public static class Сir
+        {
+            public static double Per(int rad)
+            {
+                double result = 2 * Math.PI * rad;
+                return result;
+            }
+            public static double Sq(int rad)
+            {
+                double result = Math.PI * rad * rad;
+                return result;
+            }
+            public static bool Aff(int x, int y, int rad)
+            {
+                if (((x * x) + (y * y)) <= (rad * rad))
+                {
+                    bool result = true;
+                    return result;
+                }
+                else
+                {
+                    bool result = false;
+                    return result;
+                }
+            }
+
+        }
+    }
+}
